31e76f660c25566e6922378f89c354c7aa5f872d
FernandoAltamirano <fernando.altamirano@unmsm.edu.pe>
2023-04-07 17:05:42 -0500

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { Footer, Header, Home, Social } from "./modules"
 function App() {
 
   return (
-    <div className="app">
+    <>
       <Header />
       <Home />
       <Social />
       <Footer />
-    </div>
+    </>
   )
 }
 
--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -1,18 +1,22 @@
+import About from '../About/About'
 import Contact from '../contact/contact'
 import './home.css'
 
 function Home() {
+
+    const Main = () => {
+        return <main>
+        <img src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="name">
+            <h1>MEILIN GUERRERO</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, minima?</p>
+        </div>
+    </main>
+    }
     return (
         <div className='home'>
-            <main>
-                <img src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                <div className="name">
-                    <h1>MEILIN GUERRERO</h1>
-                </div>
-            </main>
-            <section>
-                <h2>ABOUT SECTION</h2>
-            </section>
+            <Main/>
+            <About/>
             <Contact />
         </div>
     )
